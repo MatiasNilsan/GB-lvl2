78a8326f0c9452c96c4f857a8ebb8546799f08dd
MatiasNilsan <matiasnilsan@gmail.com>
2021-02-04 01:54:35 +1000

    components

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
+import { Products } from './Products.js';
+import { Cart } from './Cart.js';
 
 const Shop = {
+    components: {
+        Products,
+        Cart,
+    },
     data() {
         return {
             API: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses',
-            catalogUrl: '/catalogData.json',
-            products: [],
-            imgCatalog: 'https://placehold.it/200x150',
-            imgCart: 'https://placehold.it/100x50',
             searchLine: '',
-            cartProducts: [],
             isVisibleCart: false,
         }
     },
     methods: {
-        getJson(url) {
-            return fetch(url)
-                .then(result => result.json())
-                .catch(error => console.log(error));
-        },
-
         addProduct(product) {
             this.getJson(`${this.API}/addToBasket.json`)
                 .then(data => {
                     if (data.result) {
-                        let find = this.cartProducts.find(item => item.id_product === product.id_product);
+                        console.log(this.$refs.cart.cartProducts)
+                        let find = this.$refs.cart.cartProducts.find(item => item.id_product === product.id_product);
                         if (find) {
                             find.quantity++
                         } else {
@@ -33,42 +29,19 @@ const Shop = {
                                 price: product.price,
                                 quantity: 1
                             };
-                            this.cartProducts.push(cartItem);
+                            this.$refs.cart.cartProducts.push(cartItem);
                         }
                     }
                 });
         },
-
-        deleteFromCart(product) {
-            this.getJson(`${this.API}/deleteFromBasket.json`)
-                .then(data => {
-                    if (data.result) {
-                        if (product.quantity > 1) {
-                            product.quantity--
-                        } else {
-                            this.cartProducts.splice(this.cartProducts.indexOf(product), 1);
-                        }
-                    }
-                });
-        }
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => console.log(error));
+        },
     },
 
-    computed: {
-        filterProducts: function () {
-            let search = this.searchLine;
-            return this.products.filter((item) => {
-                return item.product_name.includes(search);
-            })
-        }
-    },
-    mounted() {
-        this.getJson(`${this.API + this.catalogUrl}`)
-            .then(data => {
-                for (let product of data) {
-                    this.products.push(product);
-                }
-            });
-    }
+
 };
 
 Vue.createApp(Shop).mount('#app');
